9148d1ea1cb32a0b5634df19219f5774ee343782
ander1119 <b07902060@ntu.edu.tw>
2024-04-28 18:44:19 +0800

    add summarization model

--- video_segment.py
+++ video_segment.py
@@ -103,28 +103,65 @@ class VideoSegment:
         import json
 
         def info_summarize(info):
-            prompt = f'You are given an json string that contains caption or query from frames in a video, please remove redundant information from the json string (for example, similar description in adjacent frame). And return the final response in json format. Here is json string you are asked to task:\n{info}\n'
-            return eval(self.forward('gpt3_summarize', prompt))
+            # prompt = f'You are given an json string that contains caption or query from frames in a video, please remove redundant information from the json string (for example, similar description in adjacent frame). And return the final response in json format. Please make the summarization result at most 3000 tokens. Here is json string you are asked to task:\n{info}\n'
+            prompt = f'{info}'
+            return self.forward('gpt3_summarize', prompt)
             
         with open(config.select_answer_prompt, 'r') as f:
             prompt = f.read()
         # info_formatting = '\n'.join([f"- {k}: {format_dict(v)}" for k, v in info.items()])
-        character_limit = 16000 * 4 - len(prompt)
-        chunk_limit = 9000 * 4
+        character_limit = 20000 - len(prompt)
+        chunk_limit = 10000
 
-        while len(json.dumps(info)) > character_limit:
-            summarized_info = {}
-            chunk_message = {}
-            for key, value in info.items():
-                chunk_message[key] = value
-                if len(json.dumps(chunk_message)) > chunk_limit:
-                    summarization = info_summarize(json.dumps(chunk_message))
-                    summarized_info.update(summarization)
-                    chunk_message = {}
-            info = summarized_info
+        # info = json.dumps(info)
+        # print(f'before summarization got info length: {len(json.dumps(info))}')
 
-        info_formatting = json.dumps(info)
-        prompt = prompt.format(info=info_formatting, question=question, options=options)
+        round = 0
+        summarization = ""
+        chunk_message = {}
+        for key, value in info.items():
+            chunk_message[key] = value
+            if len(json.dumps(chunk_message)) > chunk_limit:
+                chunk_summarization = info_summarize(json.dumps(chunk_message))
+                summarization += '\n' + chunk_summarization
+
+                chunk_message_str = json.dumps(chunk_message, indent=4)
+                # print(f'============== round {round} ==============')
+                # print(f'\n{chunk_message_str}\n')
+                # print(f'\n{chunk_summarization}\n')
+
+                chunk_message = {}
+        if len(chunk_message) != 0:
+            chunk_summarization = info_summarize(json.dumps(chunk_message))
+            summarization += '\n' + chunk_summarization
+
+            # chunk_message_str = json.dumps(chunk_message, indent=4)
+            # print(f'============== round {round} ==============')
+            # print(f'\n{chunk_message_str}\n')
+            # print(f'\n{chunk_summarization}\n')
+
+        info = summarization
+        round += 1
+
+        while len(info) > character_limit:
+            summarization = ""
+            for start_idx in range(0, len(info), chunk_limit):
+                chunk_message = info[start_idx:start_idx + chunk_limit]
+                chunk_summarization = info_summarize(chunk_message)
+                summarization += '\n' + chunk_summarization
+
+                # print(f'============== round {round} ==============')
+                # print(f'\n{chunk_message}\n')
+                # print(f'\n{chunk_summarization}\n')
+
+            info = summarization
+            round += 1
+
+        # info_legnth = len(json.dumps(info))
+        # print(f'after summarization got info length: {info_legnth}')
+
+        # info_formatting = json.dumps(info)
+        prompt = prompt.format(info=info, question=question, options=options)
         answer = self.forward('gpt3_general', prompt)
         
         # try:
